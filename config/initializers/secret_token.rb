53e7928fe90ba0cc5bb47353027c31f7e7e9ba90
Chris Serino <themindoverall@gmail.com>
2012-12-14 22:38:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abhorrent::Application.config.secret_token = '34749f87bc47ee9e2142cdf27843a3dad2cbcbf910f9491d39fe163b70044c759dcd4b74565c895d7c9cf63ecd06a4e391d558a7d8eeb5bae921fc710a1ead74'
